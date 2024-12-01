28937cd66da41829e6425118509fbc806784ee3a
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-12-01 14:33:32 +0100

    [NET] AoC 2024 Day 1
    Solution for day 1 (part 1) with benchmarks.

---
+++ net/AoC2024/Day1.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AoC2024;
+
+public class Day1
+{
+    public void Solve(string path)
+    { 
+        List<int> left = new List<int>();
+        List<int> right= new List<int>();
+        
+        using var fileStream = new FileStream(path, FileMode.Open);
+        using var streamReader = new StreamReader(fileStream);
+        
+        string line = String.Empty;
+        while ((line = streamReader.ReadLine()) is not null)
+        {
+            string[] parts = line.Split(" ");
+            left.Add(int.Parse(parts[0]));
+            right.Add(int.Parse(parts[3]));
+        }
+        
+        left.Sort();
+        right.Sort();
+
+        int sum = 0;
+
+        for (int i = 0; i < left.Count; i++)
+        {
+            sum += Math.Abs(left[i] - right[i]);
+        }
+        Console.WriteLine(sum);
+    }
+}
+
+public class Day1Benchmark
+{
+    private readonly Day1 _day1 = new Day1();
+
+    [Benchmark]
+    public void SolveTask1()
+    {
+        _day1.Solve("./input/day1.txt");
+    }
+    
+    [Benchmark]
+    public void SolveTask2()
+    {
+        _day1.Solve("./input/day1.txt");
+    }
+}
---
+++ net/AoC2024/Program.cs
@@ -0,0 +1,9 @@
+﻿using AoC2024;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+
+Day1 aoc2024Day1Task1 = new Day1();
+aoc2024Day1Task1.Solve("./input/day1.txt");
+
+var config = DefaultConfig.Instance;
+var summary = BenchmarkRunner.Run<Day1Benchmark>(config, args);
