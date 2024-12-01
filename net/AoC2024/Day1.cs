27b2ea1ca88fc9d5dc4226ea129d71cda9db4fd7
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-12-01 14:47:20 +0100

    [NET] AoC 2024 Day 1
    Solution for day 1 (part 2).

--- net/AoC2024/Day1.cs
+++ net/AoC2024/Day1.cs
@@ -1,10 +1,11 @@
 ﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Runtime;
 
 namespace AoC2024;
 
 public class Day1
 {
-    public void Solve(string path)
+    public void SolvePart1(string path)
     { 
         List<int> left = new List<int>();
         List<int> right= new List<int>();
@@ -29,7 +30,33 @@ public class Day1
         {
             sum += Math.Abs(left[i] - right[i]);
         }
-        Console.WriteLine(sum);
+        Console.WriteLine($"Part 1 solution: {sum}");
+    }
+    
+    public void SolvePart2(string path)
+    { 
+        List<int> left = new List<int>();
+        List<int> right= new List<int>();
+        
+        using var fileStream = new FileStream(path, FileMode.Open);
+        using var streamReader = new StreamReader(fileStream);
+        
+        string line = String.Empty;
+        while ((line = streamReader.ReadLine()) is not null)
+        {
+            string[] parts = line.Split(" ");
+            left.Add(int.Parse(parts[0]));
+            right.Add(int.Parse(parts[3]));
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < left.Count; i++)
+        {
+            var count = right.Count(x => x == left[i]);
+            sum += left[i] * count;
+        }
+        Console.WriteLine($"Part 2 solution: {sum}");
     }
 }
 
@@ -40,12 +67,12 @@ public class Day1Benchmark
     [Benchmark]
     public void SolveTask1()
     {
-        _day1.Solve("./input/day1.txt");
+        _day1.SolvePart1("./input/day1.txt");
     }
     
     [Benchmark]
     public void SolveTask2()
     {
-        _day1.Solve("./input/day1.txt");
+        _day1.SolvePart2("./input/day1.txt");
     }
 }
--- net/AoC2024/Program.cs
+++ net/AoC2024/Program.cs
@@ -3,7 +3,9 @@ using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Configs;
 
 Day1 aoc2024Day1Task1 = new Day1();
-aoc2024Day1Task1.Solve("./input/day1.txt");
+aoc2024Day1Task1.SolvePart1("./input/day1.txt");
+aoc2024Day1Task1.SolvePart2("./input/day1.txt");
+
 
 var config = DefaultConfig.Instance;
 var summary = BenchmarkRunner.Run<Day1Benchmark>(config, args);
